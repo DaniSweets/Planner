b8c0d7840b4970ed8135c787c61a7e40ad365f8d
DaniSweets <danisweetwood@gmail.com>
2023-10-23 23:35:16 -0500

    Local Storage
    able to retrieve from local storage onload

--- script.js
+++ script.js
@@ -3,27 +3,37 @@ $(function () {
   var currentDay = document.getElementById("currentDay");
   currentDay.textContent = dayjs().format('dddd, MMMM D, YYYY');
 
-  //save text to local storage
+  $(".saveBtn").on("click", save);
+
   function save(){
-    console.log("saving");
+    var time = $(this).siblings(".hour").text();
+    var plan = $(this).siblings(".description").val();
+    localStorage.setItem(time, plan);
   }
 
-  //event listener for save button
-  $(".saveBtn").click(save);
+  function checkStorage(item){
+    var currentPlan = localStorage.getItem(item.querySelector(".hour").textContent);
+    item.querySelector("textarea").value = currentPlan;
+  }
+
+  var allBlocks = document.querySelectorAll(".time-block");
+  allBlocks.forEach(checkStorage);
 
   setInterval($("article").each(function(){
+
     var currentHour = dayjs().hour();
     var plannerHour = $(this).attr("data-hour");
 
     if (plannerHour<currentHour){
+        $(this).removeClass("present");
         $(this).addClass("past");
     } else if (plannerHour==currentHour){
+        $(this).removeClass("future");
         $(this).addClass("present");
     } else {
         $(this).addClass("future");
     }
 
   }), 300000);
-
 
 });
